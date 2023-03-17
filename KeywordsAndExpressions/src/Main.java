8072189668a3a536aa3bc7cef66f02c11c456ed9
kleberaluizio <kleberaluizio@gmail.com>
2023-03-17 17:55:30 -0300

    Using expressions

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-    int int = 5;
+    double kilometers = (100*1.609344);
+    System.out.println(kilometers);
+
+    int highScore = 50;
+    if (highScore > 25){
+        highScore = 1000 + highScore; //add bonus points
+    }
+    System.out.println("Highscore " + highScore );
+
+    int health = 100;
+    if ((health < 25) && (highScore > 1000)){
+       highScore = highScore - 1000;  
+    }
     }
 }
