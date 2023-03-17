44ff8f7e86032d368a9af3037fc69a4b7a658fd2
kleberaluizio <kleberaluizio@gmail.com>
2023-03-17 19:07:04 -0300

    Using else if

--- IfKeywordAndCodeBlocks/src/Main.java
+++ IfKeywordAndCodeBlocks/src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         boolean gameOver = true;
-        int score = 5000;
+        int score = 8000;
         int levelCompleted = 5;
         int bonus = 100;
 
         if (score < 5000){
-            System.out.println("Your score less 5000");
+            System.out.println("Your score was less 5000");
+        }else if (score == 5000){
+            System.out.println("Your score was 5000");
         }else{
-            System.out.println("Your score was equal or greater than 5000");
+            System.out.println("Your score was greater than 5000");
+
         }
     }
 }
