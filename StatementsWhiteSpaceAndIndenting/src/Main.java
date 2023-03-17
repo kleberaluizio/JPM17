b92c3e0bc9d33a7facd1b79750f0d2cb106311a7
kleberaluizio <kleberaluizio@gmail.com>
2023-03-17 18:44:41 -0300

    New Project

---
+++ StatementsWhiteSpaceAndIndenting/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int myVariable = 50;
+
+        myVariable++;
+        System.out.println(myVariable);
+        myVariable--;
+        System.out.println(myVariable);
+    }
+}
