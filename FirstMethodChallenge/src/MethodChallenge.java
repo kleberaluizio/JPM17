dd1bb4203ba9be69f1a54936ca815280dab6e650
kleberaluizio <kleberaluizio@gmail.com>
2023-03-17 22:13:53 -0300

    Challenge completed!

--- FirstMethodChallenge/src/MethodChallenge.java
+++ FirstMethodChallenge/src/MethodChallenge.java
@@ -13,19 +13,16 @@ public class MethodChallenge {
     public static void displayHighScorePosition(String playerName,int playerPosition){
         System.out.println(playerName + " managed to get into position "+playerPosition+" on the high score list.");
     }
-    public static int calculateHighScorePosition(int playerScore){
-        int result = 0;
 
-        if  (playerScore >= 1000){
-            result = 1;
-        }else if ((playerScore >= 500) && (playerScore < 1000)){
-            result = 2;
-
-        }else if((playerScore >= 100) && (playerScore < 500)){
-            result =3;
-        }else{
-            result = 4;
+    public static int calculateHighScorePosition(int playerScore) {
+        int position = 4
+        if (playerScore >= 1000) {
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100) {
+            position = 3;
         }
-        return result;
+        return position;
     }
 }
