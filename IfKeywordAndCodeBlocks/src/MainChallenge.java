20f6bb774affc36ed015daba917987387c380445
kleberaluizio <kleberaluizio@gmail.com>
2023-03-17 21:29:49 -0300

    More on Methods

--- IfKeywordAndCodeBlocks/src/MainChallenge.java
+++ IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -1,39 +1,23 @@
 public class MainChallenge {
     public static void main(String[] args) {
 
-        boolean gameOver = true;
-        int score = 800 ;
-        int levelCompleted = 5;
-        int bonus = 100;
-
-        int finalScore = score;
-
-        if (gameOver){
-            finalScore += (levelCompleted*bonus);
-            System.out.println("Your final score was "+ finalScore);
-        }
-
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        finalScore = score;
-
-        if (gameOver){
-            finalScore += (levelCompleted*bonus);
-            System.out.println("Your final score was "+ finalScore);
-        }
-
         calculateScore(true, 800, 5, 100);
         calculateScore(true, 10000, 8, 200);
 
+        int yearOfBirth = 1993;
+        int age = calculateMyAge(yearOfBirth);
+        System.out.println("I was born in "+yearOfBirth+ ", I am " + age + " years old.");
     }
     public static void calculateScore(boolean gameOver,int score, int levelCompleted,int bonus){
 
         int finalScore = score;
+
         if (gameOver){
             finalScore += (levelCompleted*bonus);
             System.out.println("Your final score was "+ finalScore);
         }
+    }
+    public static int calculateMyAge(int yearOfBirth){
+        return (2023-yearOfBirth);
     }
 }
