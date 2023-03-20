40fa1fe82528c3e7b055c953719f60d01e8ee6df
kleberaluizio <kleberaluizio@gmail.com>
2023-03-20 10:45:32 -0300

    OverloadChallenge method to convert lenght

---
+++ OverloadedChallenge/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("68 in = " + convertToCentimeters(68) + " cm");
+        System.out.println("5ft, 8 in = " + convertToCentimeters(5,8) + " cm");
+    }
+    public static double convertToCentimeters(int heightInInch){
+
+        return heightInInch * 2.54;// 1 inch = 2.54 cm
+    }
+    public static double convertToCentimeters(int feet, int inches){
+        
+        return convertToCentimeters((feet * 12) + inches);
+    }
+}
