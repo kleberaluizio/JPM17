f7e035a0430daa24cfd08bb9f1ba01dc144f02af
kleberaluizio <kleberaluizio@gmail.com>
2023-03-22 15:57:04 -0300

    Traditional Switch challenge completed

---
+++ SwitchChallange/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char letter = 'D';
+
+        switch (letter){
+            case 'A':
+                System.out.println("A is Able");
+                break;
+            case 'B':
+                System.out.println("B is Baker");
+                break;
+            case 'C':
+                System.out.println("C is Charlie");
+                break;
+            case 'D':
+                System.out.println("D is Dog");
+                break;
+            case 'E':
+                System.out.println("E is Easy");
+                break;
+            default:
+                System.out.println("Letter " + letter + " was not found in the switch!");
+
+        }
+    }
+}
