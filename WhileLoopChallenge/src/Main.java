951aee03e850a1804c1f225c61c60ee27f295dd7
kleberaluizio <kleberaluizio@gmail.com>
2023-03-23 12:05:57 -0300

    While loop challenge completed

---
+++ WhileLoopChallenge/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 4;
+        int evenCount = 0;
+        int oddCount = 0;
+        while (i <= 20){
+            i ++;
+            if (!isEvenNumber(i)) {
+                oddCount ++;
+                continue;
+            }
+            System.out.println(i + " is even !");
+            evenCount ++;  
+            if (evenCount >= 5){
+                break;
+            }
+        }
+        System.out.println("Found " + evenCount + " even numbers and "+ oddCount + " odd numbers!");
+    }
+
+    public static boolean isEvenNumber (int number){
+        if (number % 2 == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
