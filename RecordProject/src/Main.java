6fdd0e90851838e6d6460047ef1471f12ff97eae
kleberaluizio <kleberaluizio@gmail.com>
2023-03-31 07:43:21 -0300

    Undestanding record data type

--- RecordProject/src/Main.java
+++ RecordProject/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String [] args){
         for(int i = 1; i <= 5; i++){
-            Student s = new Student("S92300" + i,
+            LPAStudent s = new LPAStudent("S92300" + i,
                     switch (i){
                         case 1: yield  "Mary";
                         case 2: yield "Carol";
@@ -13,7 +13,18 @@ public class Main {
                     "05/11/1985",
                     "Java Masterclass");
             System.out.println(s);
-
         }
+        Student pojoStudent = new Student("S923006","Ann","05/11/1985","Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent("S923007","Bill","05/11/1985","Java Masterclass");
+        System.out.println("-----------------");
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 82");
+        // recordStudent.setClassList(recordStudent.getClassList() + ", Java OCP Exam 82");
+        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList());
+
+
     }
 }
