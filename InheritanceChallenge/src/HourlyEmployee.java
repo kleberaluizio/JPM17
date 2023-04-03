910315cbca5e60a129c01ee6b74ad28b994a7af5
kleberaluizio <kleberaluizio@gmail.com>
2023-04-03 11:20:56 -0300

    Inheritance challenge completed

--- InheritanceChallenge/src/HourlyEmployee.java
+++ InheritanceChallenge/src/HourlyEmployee.java
@@ -1,17 +1,20 @@
 public class HourlyEmployee extends Employee{
     private double hourlyPayRate;
 
-    public HourlyEmployee(double hourlyPayRate) {
+    public HourlyEmployee(String name, String birthDate, String hireData, double hourlyPayRate) {
+        super(name, birthDate, hireData);
         this.hourlyPayRate = hourlyPayRate;
     }
 
+    @Override
+    public double collectPay(){
+        double payCheck = 40 * hourlyPayRate;
+        return payCheck;
 
+    }
 
     public double getDoublePay(){
-        return 2*(getHourlyPayRate());
+        return 2 * collectPay();
     }
 
-    public double getHourlyPayRate(){
-        return this.hourlyPayRate;
-    }
 }
