610ce53d4cf20a37438b7ada3fc1e0eb178fa8bb
kleberaluizio <kleberaluizio@gmail.com>
2023-04-07 19:18:04 -0300

    Learning about Factory method design pattern

--- Polymorphism/src/Main.java
+++ Polymorphism/src/Main.java
@@ -1,16 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        Movie theMovie = new Adventure("I'm a legend");
+/*        Movie theMovie = new Adventure("I'm a legend");
         theMovie.watchMovie();
         System.out.println("--".repeat(10));
         Movie theMovie1 = new Comedy("Big People");
         theMovie1.watchMovie();
         System.out.println("--".repeat(10));
         Movie theMovie2 = new ScienceFiction("Star Wars");
-        theMovie2.watchMovie();
+        theMovie2.watchMovie();*/
 
-
+        Movie theMovie = Movie.getMovie("Science", "Star Wars");
+        theMovie.watchMovie();
     }
 }
 
--- Polymorphism/src/Movie.java
+++ Polymorphism/src/Movie.java
@@ -9,4 +9,18 @@ public class Movie {
         String instaceType = this.getClass().getSimpleName();
         System.out.println(title + " is a " + instaceType + " film.");
     }
+    // This is a Factory method!
+    /*
+    * The calling code does not need to know anything about movie's subclasses.
+    * This method return a new instance object is known as Factory method in software programing design patterns */
+
+    public static Movie getMovie (String type, String title){
+        return switch(type.toUpperCase().charAt(0)){
+
+            case 'A'-> new Adventure(title);
+            case 'C'-> new Comedy(title);
+            case 'S'-> new ScienceFiction(title);
+            default -> new Movie(title);
+        };
+    }
 }
