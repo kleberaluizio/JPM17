3f49e68ab2673a70e72627930f943411d17927b0
kleberaluizio <kleberaluizio@gmail.com>
2023-04-10 11:34:02 -0300

    In progress

--- BillsBurgerChallenge/src/Main.java
+++ BillsBurgerChallenge/src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-    
-    while (true) {
-        MealOrder meal = new MealOrder();
-        meal.orderingMeal();
 
-    }
+    Item coke = new Item("drink","coke",1.50);
+    coke.printItem();
+    coke.setSize("LARGE");
+    coke.printItem();
+
+    Item avocado = new Item("Topping", "Avocado", 1.50);
+    avocado.printItem();
+
     }
 }
