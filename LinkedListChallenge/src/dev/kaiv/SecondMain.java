6714231039bfbfc719b4b017c4eb9940754f0dee
kleberaluizio <kleberaluizio@gmail.com>
2023-04-17 09:41:03 -0300

    LinkedList Challenge Completed

--- LinkedListChallenge/src/dev/kaiv/SecondMain.java
+++ LinkedListChallenge/src/dev/kaiv/SecondMain.java
@@ -19,19 +19,72 @@ public class SecondMain {
 
         Place adelaide = new Place("Adelaide", 1374);
         addPlace(placesToVisit,adelaide);
-        Place aliceSprings = new Place("Alice Springs", 2771);
-        addPlace(placesToVisit,aliceSprings);
-        Place brisbane = new Place("Brisbane", 917);
-        addPlace(placesToVisit,brisbane);
-        Place darwin = new Place("Darwin", 3972);
-        addPlace(placesToVisit,darwin);
-        Place melbourne = new Place("Melbourne", 877);
-        addPlace(placesToVisit,melbourne);
-        Place perth = new Place("Perth", 3923);
-        addPlace(placesToVisit,perth);
-        addPlace(placesToVisit,perth);
+        addPlace(placesToVisit,new Place("Adelaide", 1374));
+        addPlace(placesToVisit,new Place("Alice Springs", 2771));
+        addPlace(placesToVisit,new Place("Brisbane", 917));
+        addPlace(placesToVisit,new Place("Darwin", 3972));
+        addPlace(placesToVisit,new Place("Melbourne", 877));
+        addPlace(placesToVisit, new Place("Perth", 3923));
 
+        placesToVisit.addFirst(new Place("Sydney",0));
         System.out.println(placesToVisit);
+
+        var iterator = placesToVisit.listIterator();
+        Scanner scanner = new Scanner(System.in);
+        boolean quitLoop = false;
+        boolean forward = true;
+
+        printMenu();
+        while(!quitLoop) {
+            if (!iterator.hasPrevious()){
+                System.out.println("Originating : " + iterator.next());
+                forward = true;
+            }
+            if (!iterator.hasNext()){
+                System.out.println("Final : " + iterator.previous());
+                forward = false;
+            }
+
+            System.out.print("Enter Value: ");
+            String menuItem = scanner.nextLine().toUpperCase().substring(0,1);
+            switch (menuItem){
+                case "F":
+                    System.out.println("User wants to go forward");
+                    if (!forward){     // Reversing Direction
+                        forward = true;
+                        if (iterator.hasNext()){
+                            iterator.next(); // Adjust position forward
+                        }
+                    }
+                    if (iterator.hasNext()){
+                        System.out.println(iterator.next());
+                    }
+                    break;
+
+                case "B":
+                    System.out.println("User wants to go backward");
+                    if (forward){     // Reversing Direction
+                        forward = false;
+                        if (iterator.hasPrevious()){
+                            iterator.previous(); // Adjust position forward
+                        }
+                    }
+                    if (iterator.hasPrevious()){
+                        System.out.println(iterator.previous());
+                    }
+                    break;
+                case "L":
+                    System.out.println(placesToVisit);
+                    break;
+                case "M":
+                    printMenu();
+                    break;
+                default:
+                    quitLoop = true;
+                    break;
+            }
+        }
+
     }
 
     private static void addPlace(LinkedList<Place> list, Place place){
@@ -45,6 +98,28 @@ public class SecondMain {
                 return;
             }
         }
+
+        int matchedIndex = 0;
+        for (var listPlace: list){
+            if (place.distance() < listPlace.distance()){
+                list.add(matchedIndex,place);
+                return;
+            }
+
+            matchedIndex++;
+        }
+
         list.add(place);
+    }
+    private static void printMenu(){
+        String message = """
+                Available actions (select word or letter):
+                (F)orward
+                (B)ackward
+                (L)ist Places
+                (M)enu
+                (Q)uit
+                """;
+        System.out.println(message);
     }
 }
