f72c61ac28089b0ef267fbaed5711919dc483bf4
kleberaluizio <kleberaluizio@gmail.com>
2023-04-19 18:17:18 -0300

    A class can implements multiples interfaces

--- Interfaces/src/com/kleberaluizio/Main.java
+++ Interfaces/src/com/kleberaluizio/Main.java
@@ -3,5 +3,19 @@ package com.kleberaluizio;
 public class Main {
     public static void main(String[] args) {
 
+        ITelephone timsPhone;
+        timsPhone = new DeskPhone(123456);
+        timsPhone.powerOn();
+        timsPhone.callPhone(123456);
+        timsPhone.answer();
+
+        System.out.println("-".repeat(20));
+
+        timsPhone = new MobilePhone(123456);
+        timsPhone.powerOn();
+        timsPhone.callPhone(24565);
+        timsPhone.answer();
+
+
     }
 }
