c70f88a6551e971999bddf86046aebc56239e121
kleberaluizio <kleberaluizio@gmail.com>
2023-04-25 16:34:58 -0300

    Generic Class part 4

--- GenericClasse/src/com/kleberaluizio/Team.java
+++ GenericClasse/src/com/kleberaluizio/Team.java
@@ -2,12 +2,14 @@ package com.kleberaluizio;
 
 import java.util.ArrayList;
 
-public class Team<T extends Player> {
+public class Team<T extends Player> implements Comparable<Team<T>> {
     private String name;
     int played = 0;
     int won = 0;
     int lost = 0;
     int tied = 0;
+
+
 
     private ArrayList<T> members = new ArrayList<>();
 
@@ -57,5 +59,16 @@ public class Team<T extends Player> {
 
     public int ranking() {
         return (won * 2 ) + tied;
+    }
+
+    @Override
+    public int compareTo(Team<T> team) {
+        if (this.ranking() > team.ranking()){
+            return -1;
+        } else if(this.ranking() < team.ranking()){
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
