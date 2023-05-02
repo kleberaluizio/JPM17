53989d0e93cc30c1fc6fe8401551c26ddc3fb36f
kleberaluizio <kleberaluizio@gmail.com>
2023-05-02 09:55:44 -0300

    Scope - part 2

--- Scope/src/com/kleberaluizio/Main.java
+++ Scope/src/com/kleberaluizio/Main.java
@@ -2,15 +2,19 @@ package com.kleberaluizio;
 
 public class Main {
     public static void main(String[] args) {
-        String privateVar = "this is private to main()";
+        String varFour = "this is private to main()";
 
         ScopeCheck scopeInstance = new ScopeCheck();
-        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
-        System.out.println(privateVar);
+        scopeInstance.useInner();
 
-        scopeInstance.timesTwo();
-        System.out.println("*".repeat(30 ));
         ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
-        innerClass.timesTwo();
+        System.out.println("varThree is not accessible here " +innerClass.varThree);
+//        System.out.println("scopeInstance varOne is " + scopeInstance.getVarOne());
+//        System.out.println(varFour);
+//
+//        scopeInstance.timesTwo();
+//        System.out.println("*".repeat(30 ));
+//        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+//        innerClass.timesTwo();
     }
 }
