1bf00e915e2a662c13f8334221129824b8a70f6d
kleberaluizio <kleberaluizio@gmail.com>
2023-05-03 20:47:52 -0300

    The final statement - part 2

--- Final/src/com/kleberaluizio/Main.java
+++ Final/src/com/kleberaluizio/Main.java
@@ -2,27 +2,32 @@ package com.kleberaluizio;
 
 public class Main {
     public static void main(String[] args) {
-        SomeClass one = new SomeClass("one");
-        SomeClass two = new SomeClass("two");
-        SomeClass three = new SomeClass("three");
-
-        System.out.println(one.getInstanceNumber());
-        System.out.println(two.getInstanceNumber());
-        System.out.println(three.getInstanceNumber());
-
-//        one.instanceNumber = 4;
-        System.out.println(Math.PI);
-//        Math m = new Math();
-
-        int pw = 674312;
-        Password password = new Password(pw);
-        password.storePassword();
-
-        password.letMeIn(1);
-        password.letMeIn(54543);
-        password.letMeIn(00);
-        password.letMeIn(pw);
-
+//        SomeClass one = new SomeClass("one");
+//        SomeClass two = new SomeClass("two");
+//        SomeClass three = new SomeClass("three");
+//
+//        System.out.println(one.getInstanceNumber());
+//        System.out.println(two.getInstanceNumber());
+//        System.out.println(three.getInstanceNumber());
+//
+////        one.instanceNumber = 4;
+//        System.out.println(Math.PI);
+////        Math m = new Math();
+//
+//        int pw = 674312;
+//        Password password = new ExtendedPassword(pw);
+//        password.storePassword();
+//
+//        password.letMeIn(1);
+//        password.letMeIn(54543);
+//        password.letMeIn(00);
+//        password.letMeIn(-1);
+//        password.letMeIn(pw);
+//        password.
+        System.out.println("Main Method Called");
+        SIBTest test = new SIBTest();
+        test.someMethod();
+        System.out.println("Owner is " + SIBTest.owner);
     }
 
 }
