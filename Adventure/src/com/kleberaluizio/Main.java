921841978421fa19952743b0417eda33d71edd09
kleberaluizio <kleberaluizio@gmail.com>
2023-05-07 11:50:50 -0300

    Undestanding the concept of immultable classes

--- Adventure/src/com/kleberaluizio/Main.java
+++ Adventure/src/com/kleberaluizio/Main.java
@@ -12,27 +12,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brigde"));
-        locations.put(2, new Location(2, "You are at the top of a hill"));
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4, "You are  in a valley beside a stream"));
-        locations.put(5, new Location(5, "You are in the forest"));
-
-        locations.get(1).addExit("W",2);
-        locations.get(1).addExit("E",3);
-        locations.get(1).addExit("S",4);
-        locations.get(1).addExit("N",5);
-
-        locations.get(2).addExit("N",5);
-
-        locations.get(3).addExit("W",1);
-
-        locations.get(4).addExit("N",1);
-        locations.get(4).addExit("W",2);
-
-        locations.get(5).addExit("S",1);
-        locations.get(5).addExit("W",2);
+//        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
+//        locations.put(1, new Location(1, "You are standing at the end of a road before a small brigde"));
+//        locations.put(2, new Location(2, "You are at the top of a hill"));
+//        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
+//        locations.put(4, new Location(4, "You are  in a valley beside a stream"));
+//        locations.put(5, new Location(5, "You are in the forest"));
+//
+//        locations.get(1).addExit("W",2);
+//        locations.get(1).addExit("E",3);
+//        locations.get(1).addExit("S",4);
+//        locations.get(1).addExit("N",5);
+//
+//        locations.get(2).addExit("N",5);
+//
+//        locations.get(3).addExit("W",1);
+//
+//        locations.get(4).addExit("N",1);
+//        locations.get(4).addExit("W",2);
+//
+//        locations.get(5).addExit("S",1);
+//        locations.get(5).addExit("W",2);
 
         int loc = 1;
 
