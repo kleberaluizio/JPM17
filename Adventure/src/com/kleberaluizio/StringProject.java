0a8c73cad41452162929041bed9cd513fcc7a69e
kleberaluizio <kleberaluizio@gmail.com>
2023-05-07 10:22:31 -0300

    Using Hashmaps

---
+++ Adventure/src/com/kleberaluizio/StringProject.java
@@ -0,0 +1,17 @@
+package com.kleberaluizio;
+
+public class StringProject {
+    public static void main(String[] args) {
+        String[] road = "You are stangin at the end of a road before a small brick building".split(" ");
+        for(String i : road){
+            System.out.println(i);
+        }
+        System.out.println("=".repeat(100));
+
+        String[] building = "You are inside a building, a well house for a small spring".split(", ");
+        for(String i : building){
+            System.out.println(i);
+        }
+        System.out.println("=".repeat(100));
+    }
+}
