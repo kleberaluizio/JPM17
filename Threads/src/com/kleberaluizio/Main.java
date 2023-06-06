82d08ff51e76f22fef30c4f4c10b68b5c5e732a3
kleberaluizio <kleberaluizio@gmail.com>
2023-06-06 18:33:12 -0300

    Interrupted and Join Threads

--- Threads/src/com/kleberaluizio/Main.java
+++ Threads/src/com/kleberaluizio/Main.java
@@ -20,10 +20,18 @@ public class Main {
         Thread myRunnableThread = new Thread(new MyRunnable() {
             @Override
             public void run() {
-                System.out.println(ANSI_RED + "Hello from the anonymous class's implementaion of run()!");;
+                System.out.println(ANSI_RED + "Hello from the anonymous class's implementaion of run()!");
+                try {
+                    anotherThread.join(2000); // From here, myRunnable will wait for AnotherThread to finish.
+                    System.out.println(ANSI_RED +"AnotherThread terminated or timed out, so I'm running again");
+                } catch (InterruptedException e) {
+                    System.out.println(ANSI_RED + "I couldn't wait after all. I was interrupted");
+                }
             }
         });
+
         myRunnableThread.start();
+
         System.out.println(ANSI_PURPLE + "Hello again from the main thread.");
 
 
